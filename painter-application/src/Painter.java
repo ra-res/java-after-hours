798a74e7c47a67a1e0fba71b31766a458315d966
rarespurtan <purtan.rares@yahoo.com>
2021-04-12 21:22:55 +0100

    painter menu items

--- painter-application/src/Painter.java
+++ painter-application/src/Painter.java
@@ -1,126 +1,124 @@
 import java.awt.*;
 import java.awt.event.*;
 
-public class Painter extends Frame implements ActionListener, MouseListener, MouseMotionListener {
-    private static final long serialVersionUID = 1L;
-    private MenuBar menubar;
-    private Menu menu1, menu2, menu3;
-    private MenuItem newMenuItem, openMenuItem, saveMenuItem, colourMenuItem, exitMenuItem;
+public class Painter extends Frame
+    implements ActionListener, MouseListener, MouseMotionListener, ItemListener {
+  private static final long serialVersionUID = 1L;
+  private MenuBar menubar;
+  private Menu menu1, menu2, menu3;
+  private MenuItem newMenuItem, openMenuItem, saveMenuItem, colourMenuItem, exitMenuItem;
+  private CheckboxMenuItem linesMenuItem,
+      ellipsesMenuItem,
+      rectanglesMenuItem,
+      roundMenuItem,
+      freehandMenuItem;
 
-    private CheckboxMenuItem linesMenuItem;
-
-    public Painter() {
-        setLayout(null);
-        addMouseListener(this);
-        addMouseMotionListener(this);
-        generateMenuBar();
-        setMenuBar(menubar);
-    }
-
-    private void generateMenuBar() {
-        menubar = new MenuBar();
-        menu1 = new Menu("File");
-        menu2 = new Menu("Draw");
-        menu3 = new Menu("Effects");
-
-        // newMenuItem = initMenuItem("New", menu1);
-        // openMenuItem = initMenuItem("Open...", menu1);
-        // saveMenuItem = initMenuItem("Save As...", menu1);
-        // colourMenuItem = initMenuItem("Select Colour", menu3);
-        // exitMenuItem = initMenuItem("Exit", menu1);
-
-
-        newMenuItem = new MenuItem("New");
-        menu1.add(newMenuItem);
-        newMenuItem.addActionListener(this);
-
-        openMenuItem = new MenuItem("Open...");
-        menu1.add(openMenuItem);
-        openMenuItem.addActionListener(this);
-
-        saveMenuItem = new MenuItem("Save As...");
-        menu1.add(saveMenuItem);
-        saveMenuItem.addActionListener(this);
-
-        colourMenuItem = new MenuItem("Select Colour");
-        menu3.add(colourMenuItem);
-        colourMenuItem.addActionListener(this);
-
-        exitMenuItem = new MenuItem("Exit");
-        menu1.add(exitMenuItem);
-        exitMenuItem.addActionListener(this);
-
-        menubar.add(menu1);
-        menubar.add(menu2);
-        menubar.add(menu3);
-        setMenuBar(menubar);
-
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
+  public Painter() {
+    setLayout(null);
+    addMouseListener(this);
+    addMouseMotionListener(this);
+    addWindowListener(
+        new WindowAdapter() {
+          public void windowClosing(WindowEvent e) {
+            dispose();
+          }
         });
-        setSize(200, 200);
-        setVisible(true);
+    generateMenuBar();
+    setVisible(true);
+    setSize(400, 400);
+  }
 
-    }
+  private void generateMenuBar() {
 
-    private MenuItem initMenuItem(String name, Menu menu) {
-        MenuItem tempMenuItem = new MenuItem(name);
-        menu.add(tempMenuItem);
-        tempMenuItem.addActionListener(this);
-        return tempMenuItem;
-    }
+    menubar = new MenuBar();
+    menu1 = new Menu("File");
+    menu2 = new Menu("Draw");
+    menu3 = new Menu("Effects");
 
-    public static void main(String[] args) {
-        new Painter();
-    }
+    newMenuItem = initMenuItem("New", menu1);
+    openMenuItem = initMenuItem("Open...", menu1);
+    saveMenuItem = initMenuItem("Save As...", menu1);
+    colourMenuItem = initMenuItem("Select Colour", menu3);
+    exitMenuItem = initMenuItem("Exit", menu1);
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+    linesMenuItem = initCheckBoxMenuItem("Draw lines", menu2);
+    ellipsesMenuItem = initCheckBoxMenuItem("Draw ellipses", menu2);
+    rectanglesMenuItem = initCheckBoxMenuItem("Draw ellipses", menu2);
+    roundMenuItem = initCheckBoxMenuItem("Draw round rectangles", menu2);
+    freehandMenuItem = initCheckBoxMenuItem("Draw freehand", menu2);
 
-    }
+    menubar.add(menu1);
+    menubar.add(menu2);
+    menubar.add(menu3);
+    setMenuBar(menubar);
+  }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stub
+  private CheckboxMenuItem initCheckBoxMenuItem(String name, Menu menu) {
+    CheckboxMenuItem tempCheckboxItem = new CheckboxMenuItem(name);
+    menu.add(tempCheckboxItem);
+    tempCheckboxItem.addItemListener(this);
+    return tempCheckboxItem;
+  }
 
-    }
+  private MenuItem initMenuItem(String name, Menu menu) {
+    MenuItem tempMenuItem = new MenuItem(name);
+    menu.add(tempMenuItem);
+    tempMenuItem.addActionListener(this);
+    return tempMenuItem;
+  }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
+  public static void main(String[] args) {
+    new Painter();
+  }
 
-    }
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
+  }
 
-    }
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
+  }
 
-    }
+  @Override
+  public void mousePressed(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
+  }
 
-    }
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        // TODO Auto-generated method stub
+  }
 
-    }
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        // TODO Auto-generated method stub
+  }
 
-    }
+  @Override
+  public void mouseExited(MouseEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void mouseDragged(MouseEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void mouseMoved(MouseEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void itemStateChanged(ItemEvent e) {}
 }
